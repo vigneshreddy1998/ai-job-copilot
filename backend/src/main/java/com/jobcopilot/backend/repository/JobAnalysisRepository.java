423bd175d0bcf56039e4b3d417ae1c7b53198b5b
Vigneshreddy Kaligota <vigneshreddykaligota@gmail.com>
2026-05-30 13:37:36 -0400

    Remove application.yaml from tracking, update gitignore

--- backend/src/main/java/com/jobcopilot/backend/repository/JobAnalysisRepository.java
+++ backend/src/main/java/com/jobcopilot/backend/repository/JobAnalysisRepository.java
@@ -3,6 +3,5 @@ package com.jobcopilot.backend.repository;
 import com.jobcopilot.backend.entity.JobAnalysisEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JobAnalysisRepository
-        extends JpaRepository<JobAnalysisEntity, Long> {
+public interface JobAnalysisRepository extends JpaRepository<JobAnalysisEntity, Long> {
 }
